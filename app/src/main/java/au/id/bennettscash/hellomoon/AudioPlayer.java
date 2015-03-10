88adbb846d105aac356403727325675cdc3da664
Chris Bennetts-Cash <chris@bennettscash.id.au>
2015-03-11 06:46:34 +1100

    ch13

---
+++ app/src/main/java/au/id/bennettscash/hellomoon/AudioPlayer.java
@@ -0,0 +1,33 @@
+package au.id.bennettscash.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by chris on 11/03/15.
+ */
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context c) {
+        stop();
+
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+
+        mPlayer.start();
+    }
+}
