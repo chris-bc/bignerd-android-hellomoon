5b7297a71f13b7e5361b9b8f1184f9d5b736873f
Chris Bennetts-Cash <chris@bennettscash.id.au>
2015-03-18 13:26:58 +1100

    unsure why video not working

--- app/src/main/java/au/id/bennettscash/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/au/id/bennettscash/hellomoon/HelloMoonFragment.java
@@ -1,11 +1,17 @@
 package au.id.bennettscash.hellomoon;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.VideoView;
+
+import java.io.File;
 
 /**
  * Created by chris on 11/03/15.
@@ -14,6 +20,7 @@ public class HelloMoonFragment extends Fragment {
     private Button mPlayButton;
     private Button mStopButton;
     private Button mPauseButton;
+    private VideoView mVideoView;
     private AudioPlayer mPlayer = new AudioPlayer();
 
     @Override
@@ -41,6 +48,17 @@ public class HelloMoonFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 mPlayer.togglePause();
+            }
+        });
+
+        mVideoView = (VideoView)v.findViewById(R.id.hellomoon_video);
+        Uri uri = Uri.parse("android.resource://" + getActivity().getPackageName() + File.separator + R.raw.apollo_17_stroll);
+        mVideoView.setVideoURI(uri);
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mVideoView.start();
             }
         });
 
